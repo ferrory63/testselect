0392433f49df02aa7fdc044b24651ca9df967dbf
ferrory63 <ferrorylion@gmail.com>
2022-06-22 00:02:41 +0700

    finalfinal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Select from './Select'
 function App() {
   
 
-  const styles = ["large", "medium"]
 
   const array = [
     {id:1,text : 'Сергей', imageUrl : "/icons/photo.svg"},
@@ -21,7 +20,6 @@ function App() {
     <Select 
       content={array} 
       prompt={'Select smth'}
-      //onSelectClick={val => setValue((prev) => [...prev,val])} 
       value = {value}
       setValue = {(val) => setValue(val)}
       color = {'dark'}
